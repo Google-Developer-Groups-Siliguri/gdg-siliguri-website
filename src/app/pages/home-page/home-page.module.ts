7fead4f0408eeb7522146dce4a99e163fc22a731
Debajyoti sarkar <debajyotisarkar.jio1@gmail.com>
2023-10-03 18:05:23 +0530

    fixed font

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { OrganizersComponent } from 'src/app/components/organizers/organizers.component';
+import { AboutUsComponent } from 'src/app/components/about-us/about-us.component';
 
 const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent,OrganizersComponent,AboutUsComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomePageModule {}
