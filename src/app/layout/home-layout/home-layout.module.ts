dd1954350876d9ec877001c4ff604d9ae3ed0481
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-03 18:32:55 +0530

    welcome & footer section, angular material remove

--- src/app/layout/home-layout/home-layout.module.ts
+++ src/app/layout/home-layout/home-layout.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeLayoutComponent } from './home-layout.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 const routes: Routes = [{ path: '', component: HomeLayoutComponent }];
 
 @NgModule({
   declarations: [HomeLayoutComponent],
-  imports: [CommonModule, NavbarComponent, RouterModule.forChild(routes)],
+  imports: [CommonModule, NavbarComponent, FooterComponent,RouterModule.forChild(routes)],
 })
 export class HomeLayoutModule {}
