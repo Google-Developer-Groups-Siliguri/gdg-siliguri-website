a9a3d44f69dbd4ea7832e86221e4bef0fc80f94a
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-03 18:32:55 +0530

    welcome & footer section, angular material remove

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from 'src/app/components/welcome/welcome.component';
 
 const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), WelcomeComponent],
 })
 export class HomePageModule {}
