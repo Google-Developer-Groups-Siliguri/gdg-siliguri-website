25de0eedd6a3aa8745a9b5f4c57124bd73739cae
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-05 20:26:42 +0530

    image compression

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,18 +1,17 @@
-import { HostListener,Component } from '@angular/core';
-
+import { HostListener, Component } from '@angular/core';
+import { getWindow } from 'ssr-window';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
 })
 export class AboutUsComponent {
-  public screenWidth: number=0;
-  public screenHeight: number=0;
-  @HostListener("window:resize",["$event",])
-  getScreenSize(){
-    this.screenWidth = window.innerWidth;
-    this.screenHeight = window.innerHeight;
-    console.log(this.screenWidth, this.screenHeight);
+  public screenWidth: number = 0;
+  public screenHeight: number = 0;
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.screenWidth = getWindow().innerWidth;
+    this.screenHeight = getWindow().innerHeight;
   }
   constructor() {
     this.getScreenSize();
