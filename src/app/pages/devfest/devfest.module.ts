f4d6eb5bdeaeca2a6e006f044452b0ea4494fdc0
Sagnik Ghosh <Sagnik.Ghosh@p360.com>
2023-10-10 18:38:43 -0400

    partners sponsors and association added

--- src/app/pages/devfest/devfest.module.ts
+++ src/app/pages/devfest/devfest.module.ts
@@ -4,11 +4,14 @@ import { DevfestComponent } from './devfest.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactMapComponent } from 'src/app/components/contact-map/contact-map.component';
 import { SpeakersComponent } from 'src/app/components/speakers/speakers.component';
+import { AssociationComponent } from 'src/app/components/association/association.component';
+import { SponsorsComponent } from 'src/app/components/sponsors/sponsors.component';
+import { PartnersComponent } from 'src/app/components/partners/partners.component';
 
 const routes: Routes = [{ path: '', component: DevfestComponent }];
 
 @NgModule({
   declarations: [DevfestComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), ContactMapComponent, SpeakersComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), ContactMapComponent, SpeakersComponent, AssociationComponent, SponsorsComponent, PartnersComponent],
 })
 export class DevfestModule {}
