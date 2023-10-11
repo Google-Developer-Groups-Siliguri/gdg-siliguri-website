0487e23a66eecbc3ef9edad9c863cc9478ae3e59
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-11 19:18:29 +0530

    wrap setInterval into isPlatformBrowser condition

--- src/app/components/devfest-hero/devfest-hero.component.ts
+++ src/app/components/devfest-hero/devfest-hero.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-devfest-hero',
@@ -9,24 +11,30 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./devfest-hero.component.css'],
 })
 export class DevfestHeroComponent implements OnInit {
-  ngOnInit(): void {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setInterval(() => {
+        var now = new Date().getTime();
+        var remainingTime = this.countdownDate - now;
+        this.remainingDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        this.remainingHours = Math.floor(
+          (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        this.remainingMinutes = Math.floor(
+          (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        this.remainingSeconds = Math.floor(
+          (remainingTime % (1000 * 60)) / 1000
+        );
+      });
+    }
+  }
 
   countdownDate = new Date('november 05, 2023 00:00:00').getTime();
   remainingDays: number | undefined;
   remainingHours: number | undefined;
   remainingMinutes: number | undefined;
   remainingSeconds: number | undefined;
-
-  x = setInterval(() => {
-    var now = new Date().getTime();
-    var remainingTime = this.countdownDate - now;
-    this.remainingDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    this.remainingHours = Math.floor(
-      (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    this.remainingMinutes = Math.floor(
-      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    this.remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-  });
 }
