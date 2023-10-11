0f29d36e02d09f953da0c0f2af0a8002c29bbc1e
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-11 15:24:17 +0530

    devfest hero component

--- src/app/pages/devfest/devfest.module.ts
+++ src/app/pages/devfest/devfest.module.ts
@@ -4,9 +4,9 @@ import { DevfestComponent } from './devfest.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactMapComponent } from 'src/app/components/contact-map/contact-map.component';
 import { SpeakersComponent } from 'src/app/components/speakers/speakers.component';
-import { AssociationComponent } from 'src/app/components/association/association.component';
 import { SponsorsComponent } from 'src/app/components/sponsors/sponsors.component';
 import { PartnersComponent } from 'src/app/components/partners/partners.component';
+import { DevfestHeroComponent } from 'src/app/components/devfest-hero/devfest-hero.component';
 
 const routes: Routes = [{ path: '', component: DevfestComponent }];
 
@@ -15,9 +15,9 @@ const routes: Routes = [{ path: '', component: DevfestComponent }];
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    DevfestHeroComponent,
     ContactMapComponent,
     SpeakersComponent,
-    AssociationComponent,
     SponsorsComponent,
     PartnersComponent,
   ],
