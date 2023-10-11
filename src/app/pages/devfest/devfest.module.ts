29b9c1a50351a93c1ee39181b8e49cb2ff78d89f
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-11 11:08:18 +0530

    hero section

--- src/app/pages/devfest/devfest.module.ts
+++ src/app/pages/devfest/devfest.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DevfestComponent } from './devfest.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DevfestHeroComponent } from 'src/app/components/devfest-hero/devfest-hero.component';
 
 const routes: Routes = [{ path: '', component: DevfestComponent }];
 
 @NgModule({
   declarations: [DevfestComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, DevfestHeroComponent, RouterModule.forChild(routes)],
 })
 export class DevfestModule {}
