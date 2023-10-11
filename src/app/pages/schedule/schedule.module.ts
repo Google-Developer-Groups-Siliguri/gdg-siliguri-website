e1036ba069efb997aa6609f7e74c7b785dd531a2
Das-Swarup <swarupd049@gmail.com>
2023-10-11 15:45:04 +0530

    created schedule component

--- src/app/pages/schedule/schedule.module.ts
+++ src/app/pages/schedule/schedule.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScheduleComponent } from './schedule.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TimelineComponent } from 'src/app/components/timeline/timeline.component';
 
 const routes: Routes = [{ path: '', component: ScheduleComponent }];
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), TimelineComponent],
 })
 export class ScheduleModule {}
