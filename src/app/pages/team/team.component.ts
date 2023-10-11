45a52f1ad1de501565702035af670dc02025c8fc
Sagnik Ghosh <Sagnik.Ghosh@p360.com>
2023-10-11 15:05:47 -0400

    Data passed through props for member-card

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
+  organizerDetails=[
+    {name:'Debajit Mallick', designation:'Software Engineer @P360 solutions', role: 'Organizer', image: ''},
+    {name:'Debajit Mallick', designation:'Software Engineer @P360 solutions', role: 'Organizer', image: ''},
+    {name:'Debajit Mallick', designation:'Software Engineer @P360 solutions', role: 'Organizer', image: ''},
+  ]
 
+  volunteerDetails=[
+    {name:'Sankalan Dasgupta', designation:'Software Engineer @P360 solutions', role: 'Volunteer', image: ''},
+    {name:'Sankalan Dasgupta', designation:'Software Engineer @P360 solutions', role: 'Volunteer', image: ''},
+    {name:'Sankalan Dasgupta', designation:'Software Engineer @P360 solutions', role: 'Volunteer', image: ''},
+    {name:'Sankalan Dasgupta', designation:'Software Engineer @P360 solutions', role: 'Volunteer', image: ''},
+  ]
 }
