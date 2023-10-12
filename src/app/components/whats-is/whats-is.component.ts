34fab9abd3e0f615d82f3f351e255f69988de723
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-12 21:00:32 +0530

    what is devfest section, hero section header img add, cls fix, navbar style changes

---
+++ src/app/components/whats-is/whats-is.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-whats-is',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './whats-is.component.html',
+  styleUrls: ['./whats-is.component.css']
+})
+export class WhatsIsComponent {
+
+}
