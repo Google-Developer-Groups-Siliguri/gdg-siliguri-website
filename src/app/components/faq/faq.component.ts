7933be6df98dee723b2b01c0969f4cdd5b9d8c74
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-12 15:54:23 +0530

    Faq component to change programatically

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { getDocument } from 'ssr-window';
+import {
+  Accordion,
+  AccordionInterface,
+  AccordionItem,
+  AccordionOptions,
+} from 'flowbite';
 
 @Component({
   selector: 'app-faq',
@@ -8,7 +22,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css'],
 })
-export class FaqComponent {
+export class FaqComponent implements AfterViewInit {
+  accordion: AccordionInterface | undefined;
+  accordionItems: AccordionItem[] = [];
+  options: AccordionOptions = {};
+  @ViewChildren('accordionexampleheading1') items!: QueryList<ElementRef>;
+
   data = [
     {
       question: 'What is Devfest',
@@ -23,4 +42,31 @@ export class FaqComponent {
       ans: 'You can follow us on our Social Media links which you can find below - https://linktr.ee/gdgsiliguri',
     },
   ];
+
+  /*
+   * accordionItems: array of accordion item objects
+   * options: optional
+   */
+
+  ngAfterViewInit(): void {
+    // options with default values
+    const elements = this.items.toArray();
+    elements.forEach((element: ElementRef, index) => {
+      const accordionItem = {
+        id: `accordion-example-heading-${index + 1}`,
+        triggerEl: element?.nativeElement! as HTMLElement,
+        targetEl: getDocument().querySelector(
+          `#accordion-example-body-${index + 1}`
+        )! as HTMLElement,
+        active: false,
+      };
+      this.accordionItems.push(accordionItem);
+    });
+    this.options = {
+      alwaysOpen: false,
+      activeClasses: 'text-black dark:text-white',
+      inactiveClasses: 'text-gray-500 dark:text-gray-400',
+    };
+    this.accordion = new Accordion(this.accordionItems, this.options);
+  }
 }
