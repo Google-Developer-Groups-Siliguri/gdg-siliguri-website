0619016e37fe9f094d8c86f4866fef9c3bc8078e
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-12 21:57:42 +0530

    changed devfest route from devfest/home to devfest

--- src/app/components/navbar-devfest/navbar-devfest.component.ts
+++ src/app/components/navbar-devfest/navbar-devfest.component.ts
@@ -13,11 +13,11 @@ export class NavbarDevfestComponent {
   @ViewChild('navbardefault') block: ElementRef | undefined;
 
   routes = [
-    { path: 'home', name: 'Home' },
-    { path: 'schedule', name: 'Schedule' },
-    { path: 'tickets', name: 'Tickets' },
-    { path: 'team', name: 'Team' },
-    { path: 'contact', name: 'Contact Us' },
+    { path: '/devfest', name: 'Home', exact: true },
+    { path: 'schedule', name: 'Schedule', exact: false },
+    { path: 'tickets', name: 'Tickets', exact: false },
+    { path: 'team', name: 'Team', exact: false },
+    { path: 'contact', name: 'Contact Us', exact: false },
   ];
 
   toggleClass() {
