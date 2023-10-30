a4117ebf9adc3fd7701b4ea657f5067ccbe57546
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-10-30 19:13:51 +0530

    add review section data

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import { DataService, Feedbacks } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-review',
@@ -12,25 +13,32 @@ import { register } from 'swiper/element/bundle';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ReviewComponent implements OnInit {
-  data = [
-    {
-      name: 'Eishan Chattaraj',
-      header:
-        'Attended the most innovative and informative events from the Window of Mt Kanchenjunga!',
-      content:
-        'GDG Siliguri has organised the innovative Google DevFest events at SIT where every bits was enjoyable, informative and the energy from both side of Speaker, Organizer Audience have made the GDG more Masala-dar. I have enjoyed lot and gain many knowledge for my core area of industry.',
-    },
-    {
-      name: 'Sahil Saha',
-      header: 'Awesome Experience!',
-      content:
-        'Got to learn a lot from the amazing speakers. I had a lot of fun interacting with other like minded developers from the region and participating in the hackathon! Eagerly waiting for the next Devfest!',
-    },
-  ];
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  reviewsData: Feedbacks[] = [];
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private $firebaseDataService: DataService
+  ) {
+    this.getFeedbacks();
+  }
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       register();
     }
+    console.log(this.reviewsData);
+  }
+
+  getFeedbacks() {
+    const subscription$ = this.$firebaseDataService
+      .getAllFeedbacks()
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            this.reviewsData = result;
+            subscription$.unsubscribe();
+          }
+        },
+        error: (err) => console.error(err),
+      });
   }
 }
