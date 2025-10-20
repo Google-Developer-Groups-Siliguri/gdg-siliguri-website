39ad0f37551fd7dea27463294a28b79648e2e784
Sankalan47 <sankalandasgupta@gmail.com>
2025-10-20 10:42:01 +0530

    fix netlify toml

--- src/app/components/speakers/speakers.component.ts
+++ src/app/components/speakers/speakers.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpeakerCardComponent } from '../speaker-card/speaker-card.component';
 import { ComingSoonCardComponent } from '../coming-soon-card/coming-soon-card.component';
 import { MemberCardComponent } from '../member-card/member-card.component';
 import { DataService, Members } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-speakers',
-    imports: [
-        CommonModule,
-        SpeakerCardComponent,
-        ComingSoonCardComponent,
-        MemberCardComponent,
-    ],
-    templateUrl: './speakers.component.html',
-    styleUrls: ['./speakers.component.css']
+  selector: 'app-speakers',
+  imports: [CommonModule, ComingSoonCardComponent, MemberCardComponent],
+  templateUrl: './speakers.component.html',
+  styleUrls: ['./speakers.component.css'],
 })
 export class SpeakersComponent {
   speakersDetails$: Observable<{ enabled: boolean; data: Members[] }>;
